ef300f4ff3e0899efcfe1059eae63521e5f7a06f
ryden <rythewuff@gmail.com>
2019-01-29 16:36:19 +0100

    actual titlebar support thingy for macos

--- start.js
+++ start.js
@@ -3,11 +3,21 @@ const {app, BrowserWindow} = electron;
 const path = require('path');
 const url = require('url');
 const pack = require('./package.json');
+const os = require('os')
+
+let UserOS = os.platform()
+if(UserOS === 'darwin'){
+  frame = true
+} else if (UserOS === 'win32'){
+  frame = false
+} else {
+  frame = false
+}
 
 let win;
-
 function createWindow() {
-  win = new BrowserWindow({width: 1200, height: 650, frame: true, icon: path.join(__dirname, 'images/kiwimascoticnborders.png')});
+  win = new BrowserWindow({width: 1200, height: 650, frame: frame, icon: path.join(__dirname, 'images/kiwimascoticnborders.png')});
+ // win.webContents.openDevTools();
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
